d68fc85e5cfd34dde4dedcf2333d8c8cb48558ae
Gerwin <GGGamingVL@gmail.com>
2016-10-11 15:41:58 +0200

    Added UI into my own map.
    Added the UI as it is right now.
    Not completed, placed it into my own map.

---
+++ Gaijin/Assets/Gerwin/OnOffScript.js
@@ -0,0 +1,27 @@
+﻿#pragma strict
+
+var AanUit : boolean;
+var Obj : GameObject;
+var Obj2 : GameObject;
+
+function Start () {
+	AanUit = false;
+}
+
+function Update () {
+
+
+}
+
+function Toggle () {
+	if(AanUit == false){
+		Obj.SetActive(true);
+		Obj2.SetActive(false);
+		AanUit = true;
+	}
+	else if(AanUit == true){
+			Obj.SetActive(false);
+			Obj2.SetActive(true);
+			AanUit = false;
+	}
+}
